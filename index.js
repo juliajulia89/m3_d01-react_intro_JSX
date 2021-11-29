f897202d61fc0075a5f0aaaae33fd425737ce63d
juliajulia89 <julkajanicka@gmail.com>
2021-11-29 12:05:44 +0100

    react

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-function MyNavbar(){
-    return (
-        <nav>
-    <a href="ironhack.com">Ironhack</a>
-    <br/>
-    <a href="reactjs.org">React</a>
-    </nav>
-    )
+//function MyNavbar(props) {
+  //return (
+   // <nav>
+ //     <h1>{props.mainText.toUpperCase()}</h1>
+   //   <a href="ironhack.com">Iornhack</a>
+     // <br />
+      //<a href="reactjs.org">React</a>
+    //</nav>
+ // )}
+
+//function MyFooter() {
+ // return (
+ //   <p>
+//      <span>This is the first react application for the Ovelles Negres</span>
+  //  </p>
+//  );}
+
+function PersonCard(props) {
+  return (
+    <div class="person-card">
+      <p>Name: {props.person.name}</p>
+      <p>Surname: {props.person.surname}</p>
+      <p>Age: {props.person.age}</p>
+      <p>Height: {props.person.height}</p>
+    </div>
+  );
 }
-function MyFooter(){
-    return(
-        <p><span>First paragraph with REACT</span></p>
-    )
+
+function App() {
+  const person = {
+    name: "Julia",
+    surname: "Janicka",
+    age: 32,
+    height: 170,
+  };
+
+  return <PersonCard person={person} />;
 }
-function App(){
-    return (
-        <div>
-            <MyNavbar />
-            <MyFooter />
-        </div>
-    )
-}
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
