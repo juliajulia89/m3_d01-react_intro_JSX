2f3dd71d424d234655a1c7c1618673b31a430545
juliajulia89 <julkajanicka@gmail.com>
2021-11-29 12:16:47 +0100

    react

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-//function MyNavbar(props) {
-  //return (
-   // <nav>
- //     <h1>{props.mainText.toUpperCase()}</h1>
-   //   <a href="ironhack.com">Iornhack</a>
-     // <br />
-      //<a href="reactjs.org">React</a>
-    //</nav>
- // )}
-
-//function MyFooter() {
- // return (
- //   <p>
-//      <span>This is the first react application for the Ovelles Negres</span>
-  //  </p>
-//  );}
-
+function MyNavbar(props) {
+  return (
+    <nav className={"menu"}>
+      <h1>{props.mainText.toUpperCase()}</h1>
+      <a href="https://www.ironhack.com/es">Ironhack</a>
+      <a href="https://www.ironhack.com/es">React</a>
+    </nav>
+  );
+}
+function MyFooter() {
+  return (
+    <p>
+      <span>This is the first react App for the Ovelles Negres</span>
+    </p>
+  );
+}
 function PersonCard(props) {
   return (
-    <div class="person-card">
+    <div className={"person-card"}>
       <p>Name: {props.person.name}</p>
       <p>Surname: {props.person.surname}</p>
-      <p>Age: {props.person.age}</p>
-      <p>Height: {props.person.height}</p>
+      <p>Nationality: {props.person.nationality}</p>
     </div>
   );
 }
-
 function App() {
   const person = {
     name: "Julia",
     surname: "Janicka",
-    age: 32,
-    height: 170,
+    nationality: "Polish",
   };
-
-  return <PersonCard person={person} />;
+  return (
+    <div>
+      <MyNavbar id={"my-navbar"} className={"cicio"} mainText={"heading"} />{" "}
+      {/*This is equivalent to call a function MyNavbar (mainText=heading)*/}
+      <PersonCard person={person} />
+      <MyFooter />
+    </div>
+  );
 }
 ReactDOM.render(<App />, document.getElementById("root"));
